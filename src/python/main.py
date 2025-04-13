863f184f89874380f91062429d1acf040fa8117a
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-04-13 17:30:01 +0300

    created main.py

---
+++ src/python/main.py
@@ -0,0 +1,16 @@
+from PyQt6 import QtWidgets
+
+from src.python.ui.Ui_MainWindow import Ui_MainWindow
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+
+    ui = Ui_MainWindow()
+
+    ui.setupUi(MainWindow)
+
+    MainWindow.show()
+    sys.exit(app.exec())
