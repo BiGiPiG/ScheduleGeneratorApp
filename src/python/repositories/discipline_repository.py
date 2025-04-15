a86286f10ea8a6db924c0a415b6236df9f22c2b1
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-04-15 06:42:29 +0300

    added find_all_discipline_information and find_prep functions

--- src/python/repositories/discipline_repository.py
+++ src/python/repositories/discipline_repository.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from PyQt6.QtCore import QDate
+
 
 @dataclass
 class Discipline:
@@ -81,3 +83,53 @@ class DisciplineRepository:
         except Exception as e:
             print(f"Ошибка при получении предметов дисциплин: {e}")
             return []
+
+    def find_all_discipline_information(self, group_names, date, title, work_type):
+        query = """
+                SELECT pair, d.shorttitle, sc_rasp18_rooms.room, sc_rasp18_days.day
+                FROM sc_group g
+                JOIN sc_rasp18_groups rg ON g.id = rg.group_id
+                JOIN sc_rasp18 r ON rg.rasp18_id = r.id
+                JOIN sc_disc d ON r.disc_id = d.id
+                JOIN sc_rasp18_days ON sc_rasp18_days.id = r.day_id
+                JOIN sc_worktypes ON r.worktype = sc_worktypes.id
+                JOIN sc_rasp18_rooms ON r.id = sc_rasp18_rooms.rasp18_id
+                WHERE g.title = ANY(%s) and sc_rasp18_days.day = %s and d.shorttitle = %s
+                and sc_worktypes.title = %s
+                """
+
+        try:
+            self.cursor.execute(query, (group_names, date, title, work_type))
+            return [[row[0], row[1], row[2],
+                    QDate(row[3].year, row[3].month, row[3].day).toString("yyyy-MM-dd")]
+                    for row in self.cursor.fetchall()]
+        except Exception as e:
+            print(f"Ошибка при получении информации о предмете: {e}")
+            return []
+
+    def find_prep(self, group_names, date, title, work_type):
+        query = """
+            SELECT sc_prep.fio
+            FROM sc_group g
+            JOIN sc_rasp18_groups rg ON g.id = rg.group_id
+            JOIN sc_rasp18 r ON rg.rasp18_id = r.id
+            JOIN sc_disc d ON r.disc_id = d.id
+            JOIN sc_rasp18_days rd ON r.day_id = rd.id
+            JOIN sc_worktypes wt ON r.worktype = wt.id
+            JOIN sc_rasp18_rooms rm ON r.id = rm.rasp18_id
+            JOIN sc_rasp18_preps rp ON r.id = rp.rasp18_id
+            JOIN sc_prep ON rp.prep_id = sc_prep.id
+            WHERE g.title = ANY(%s)
+              AND rd.day = %s
+              AND d.shorttitle = %s
+              AND wt.title = %s;
+        """
+
+        try:
+            self.cursor.execute(query, (group_names, date, title, work_type))
+            return [row[0]
+                    for row in self.cursor.fetchall()]
+        except Exception as e:
+            print(f"Ошибка при получении информации о предмете: {e}")
+            return []
+
